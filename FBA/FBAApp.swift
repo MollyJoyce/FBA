b745cd9cd5d72c52784fc587561bf1a1897fabde
Molly Joyce <mjoyce8695@student.pps.net>
2022-11-02 14:26:28 -0700

    Initial Commit

---
+++ FBA/FBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBAApp.swift
+//  FBA
+//
+//  Created by Molly Joyce on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
